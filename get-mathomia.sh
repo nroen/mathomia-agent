49b724efd9ec48e923494d47dfc1ddda4465f97d
Nikolai Rønningen <nikolair@gmail.com>
2026-05-30 17:47:34 +0200

    fiks payload

--- get-mathomia.sh
+++ get-mathomia.sh
@@ -25,7 +25,10 @@ INSTALL_DIR="/usr/local/bin/mathomia"
 mkdir -p "$INSTALL_DIR"
 
 echo "📥 Henter alt-i-ett-agenten fra GitHub..."
-curl -sSL "https://raw.githubusercontent.com/nroen/mathomia-agent/release/linux/agent.sh" > "$INSTALL_DIR/agent.sh"
+curl -sSL "https://raw.githubusercontent.com/nroen/mathomia-agent/release/agent.sh" -o /usr/local/bin/mathomia/agent.sh
+
+
+
 chmod +x "$INSTALL_DIR/agent.sh"
 
 echo "⏰ Setter opp cron-jobb (Kjører hver time)..."
